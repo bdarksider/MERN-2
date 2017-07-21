28d3030273c2a909596dbfa8db315db0322a5f2d
Bejoy Mathew <bejoygm@gmail.com>
2017-07-21 13:58:25 +0530

    component communication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,72 @@
+var bugs = [
+    {id:1, priority:"P1", status:"Open", owner:"Ravan", title:"App crashes on open"},
+    {id:2, priority:"P2", status:"New", owner:"Eddie", title:"Misaligned border on panel"}
+];
+
+
+class BugFilter extends React.Component {
+  render() {
+    return (
+        <div>Bug Filter</div>
+    )
+  }
+}
+
+class BugTable extends React.Component {
+  render() {
+    return (
+        <table>
+          <thead>
+              <tr>
+                <th>id</th>
+                <th>status</th> 
+                <th>priority</th>
+                <th>owner</th>
+                <th>title</th>
+              </tr>
+          </thead>
+          <tbody>
+            <BugRow id={1} priority="P1" status="Open" owner="Ravan" title="App crashes on open" />
+            <BugRow id={2} priority="P2" status="New" owner="Eddie" title="Misaligned border on panel" />
+          </tbody>
+        </table>
+    )
+  }
+}
+
+class BugAdd extends React.Component {
+  render() {
+    return (
+        <div>Add Bug</div>
+    )
+  }
+}
+
+class BugRow extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.id}</td>
+                <td>{this.props.status}</td>
+                <td>{this.props.priority}</td>
+                <td>{this.props.owner}</td>
+                <td>{this.props.title}</td>
+            </tr>
+        )
+    }
+}
+
 class BugList extends React.Component {
   render() {
     return (
-      <div>Bug List here!</div>
+      <div>
+          <h1>Bug Tracker</h1>
+          <BugFilter />
+          <hr />
+          <BugTable />
+          <hr />
+          <BugAdd />
+      </div>
     )
   }
 }
--- static/App.js
+++ static/App.js
@@ -1,9 +1,122 @@
+var bugs = [{ id: 1, priority: "P1", status: "Open", owner: "Ravan", title: "App crashes on open" }, { id: 2, priority: "P2", status: "New", owner: "Eddie", title: "Misaligned border on panel" }];
+
+class BugFilter extends React.Component {
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      "Bug Filter"
+    );
+  }
+}
+
+class BugTable extends React.Component {
+  render() {
+    return React.createElement(
+      "table",
+      null,
+      React.createElement(
+        "thead",
+        null,
+        React.createElement(
+          "tr",
+          null,
+          React.createElement(
+            "th",
+            null,
+            "id"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "status"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "priority"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "owner"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "title"
+          )
+        )
+      ),
+      React.createElement(
+        "tbody",
+        null,
+        React.createElement(BugRow, { id: 1, priority: "P1", status: "Open", owner: "Ravan", title: "App crashes on open" }),
+        React.createElement(BugRow, { id: 2, priority: "P2", status: "New", owner: "Eddie", title: "Misaligned border on panel" })
+      )
+    );
+  }
+}
+
+class BugAdd extends React.Component {
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      "Add Bug"
+    );
+  }
+}
+
+class BugRow extends React.Component {
+  render() {
+    return React.createElement(
+      "tr",
+      null,
+      React.createElement(
+        "td",
+        null,
+        this.props.id
+      ),
+      React.createElement(
+        "td",
+        null,
+        this.props.status
+      ),
+      React.createElement(
+        "td",
+        null,
+        this.props.priority
+      ),
+      React.createElement(
+        "td",
+        null,
+        this.props.owner
+      ),
+      React.createElement(
+        "td",
+        null,
+        this.props.title
+      )
+    );
+  }
+}
+
 class BugList extends React.Component {
   render() {
     return React.createElement(
-      'div',
+      "div",
       null,
-      'Bug List here!'
+      React.createElement(
+        "h1",
+        null,
+        "Bug Tracker"
+      ),
+      React.createElement(BugFilter, null),
+      React.createElement("hr", null),
+      React.createElement(BugTable, null),
+      React.createElement("hr", null),
+      React.createElement(BugAdd, null)
     );
   }
 }
