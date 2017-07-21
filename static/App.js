5dad93610fabf7ae422d21ed31d7bccd63bde66f
Bejoy Mathew <bejoygm@gmail.com>
2017-07-21 17:26:05 +0530

    get and post request for bugs

--- static/App.js
+++ static/App.js
@@ -84,7 +84,6 @@ class BugAdd extends React.Component {
     e.preventDefault();
     var form = document.forms.bugAdd;
     this.props.addBug({ owner: form.owner.value, title: form.title.value, status: 'New', priority: 'P1' });
-    // clear the form for the next input
     form.owner.value = "";
     form.title.value = "";
   }
--- webapp.js
+++ webapp.js
@@ -1,8 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
+app.use(bodyParser.json());
+
+var bugs = [
+    {id:1, priority:"P1", status:"Open", owner:"Ravan", title:"App crashes on open"},
+    {id:2, priority:"P2", status:"New", owner:"Eddie", title:"Misaligned border on panel"}
+];
+
+
 app.use(express.static('static'))
+
+app.get('/api/bugs', function (req, res) {
+  res.status(200).json(bugs);
+});
+
+app.post('/api/bugs', function(req, res) {
+    var bug = {
+        owner: req.body.owner,
+        title: req.body.title,
+        priority: "P1", 
+        status: "New",
+        id: bugs.length + 1
+    }
+
+    bugs.push(bug);
+    res.json(bug);
+})
 
 app.listen(3000, function() {
     console.log('running at 3000');
 });
+
+
+
